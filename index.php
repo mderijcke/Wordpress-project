ce3e4cb80db8263680e2b62d90fddeddc28fa57c
rolts <xxroltsxx@hotmail.nl>
2015-01-12 15:04:23 +0100

    Update showcase

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <?php get_header(); ?>
 		<!--Content-->
-		<div id="showcase">
-			<?php query_posts('&showposts=1&orderby=post_date&order=desc&cat=3'); ?>
-		
-			<?php if(have_posts()) : ?>
-
-			<?php while(have_posts()) : the_post(); ?>							   
-					
-					<h2><?php the_title(); ?></h2>	
-					<?php the_content(); ?>		
-			
-				<?php endwhile; ?>			
-		
-				<?php else: ?>			
-			
-					<?php _e('Er is nog geen bericht in deze categorie'); ?>	
-					
-			<?php endif; ?>		
-		</div>
 
 		<div id="content">
+
+			<div id="showcase">
+				<?php query_posts('&showposts=1&orderby=post_date&order=desc&cat=3'); ?>
+		
+				<?php if(have_posts()) : ?>
+
+				<?php while(have_posts()) : the_post(); ?>							   
+					
+						<h2><?php the_title(); ?></h2>	
+						<?php the_content(); ?>		
+			
+					<?php endwhile; ?>			
+		
+					<?php else: ?>			
+			
+						<?php _e('Er is nog geen bericht in deze categorie'); ?>	
+					
+				<?php endif; ?>		
+			</div>
+			
 			<!-- Laadt de berichten in -->
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 

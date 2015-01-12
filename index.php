76c54ce526cee1f941b04d0948e369661c2b8e38
rolts <xxroltsxx@hotmail.nl>
2015-01-12 14:08:26 +0100

    Add showcase

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php get_header(); ?>
 		<!--Content-->
 		<div id="content">
-			<?php query_posts('&showposts=1&orderby=post_date&order=desc&cat=4'); ?>
+			<div id="showcase">
+				<?php query_posts('&showposts=1&orderby=post_date&order=desc&cat=3'); ?>
 		
-		<?php if(have_posts()) : ?>
+				<?php if(have_posts()) : ?>
 
-			<?php while(have_posts()) : the_post(); ?>							   
+				<?php while(have_posts()) : the_post(); ?>							   
 					
-				<h2><?php the_title(); ?></h2>	
-				<?php the_content(); ?>		
+					<h2><?php the_title(); ?></h2>	
+					<?php the_content(); ?>		
 			
-			<?php endwhile; ?>			
+				<?php endwhile; ?>			
 		
-			<?php else: ?>			
+				<?php else: ?>			
 			
-				<?php _e('Er is nog geen bericht in deze categorie'); ?>	
+					<?php _e('Er is nog geen bericht in deze categorie'); ?>	
 					
-		<?php endif; ?>
+				<?php endif; ?>
+			</div>
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 			<p id="header-title">
